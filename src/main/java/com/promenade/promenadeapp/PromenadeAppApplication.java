3b0c50a7569106948782a90d73c1c15370412571
Sion Noh <shtldhs123@ajou.ac.kr>
2022-03-31 17:30:19 +0900

    Intellij 스프링 부트 프로젝트 환경 구축

---
+++ src/main/java/com/promenade/promenadeapp/PromenadeAppApplication.java
@@ -0,0 +1,13 @@
+package com.promenade.promenadeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromenadeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromenadeAppApplication.class, args);
+	}
+
+}
