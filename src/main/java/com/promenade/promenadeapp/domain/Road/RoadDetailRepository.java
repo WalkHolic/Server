0a936cada7cb9c376cbe02a0b81c661c4becb553
Sion Noh <shtldhs123@ajou.ac.kr>
2022-04-04 18:48:27 +0900

    Entity - ManyToOne(RoadPath:Road) 단방향 관계 설정

--- src/main/java/com/promenade/promenadeapp/domain/Road/RoadDetailRepository.java
+++
@@ -1,6 +0,0 @@
-package com.promenade.promenadeapp.domain.Road;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoadDetailRepository extends JpaRepository<RoadDetail, Long> {
-}
