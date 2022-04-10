d9ea6b8046c171f5266788f15365a03141116667
Sion Noh <shtldhs123@ajou.ac.kr>
2022-04-10 23:02:57 +0900

    userId로 커스텀 산책로 조회하기

--- src/main/java/com/promenade/promenadeapp/domain/User/UserRoadRepository.java
+++ src/main/java/com/promenade/promenadeapp/domain/User/UserRoadRepository.java
@@ -3,6 +3,9 @@ package com.promenade.promenadeapp.domain.User;
 import com.promenade.promenadeapp.domain.Road.Road;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRoadRepository extends JpaRepository<UserRoad, Long> {
-    UserRoad deleteByUserId(Long userId);
+
+    List<UserRoad> findByUserId(Long UserId);
 }
