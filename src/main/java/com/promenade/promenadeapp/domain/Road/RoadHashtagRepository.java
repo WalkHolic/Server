23ceb09bf4048ad253a3cb47a14769fdc70c5e06
Sion Noh <shtldhs123@ajou.ac.kr>
2022-06-01 02:06:45 +0900

    산책로 해시태그 distinct 조회

--- src/main/java/com/promenade/promenadeapp/domain/Road/RoadHashtagRepository.java
+++ src/main/java/com/promenade/promenadeapp/domain/Road/RoadHashtagRepository.java
@@ -2,12 +2,17 @@ package com.promenade.promenadeapp.domain.Road;
 
 import com.promenade.promenadeapp.domain.User.UserRoadHashtag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface RoadHashtagRepository extends JpaRepository<RoadHashtag, Long> {
 
     List<RoadHashtag> findByRoadId(Long roadId);
+
+    @Query(value = "SELECT DISTINCT hashtag FROM road_hashtag WHERE road_id=:roadId", nativeQuery = true)
+    List<String> findDistinctHashtagByRoadId(@Param("roadId") Long roadId);
 
     List<RoadHashtag> findByHashtag(String hashtag);
 
