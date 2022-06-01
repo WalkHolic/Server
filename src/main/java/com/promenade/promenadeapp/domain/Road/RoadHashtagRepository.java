1cfbaaff833e1f005f183ff5b0a363241358bcbc
Sion Noh <shtldhs123@ajou.ac.kr>
2022-06-01 15:38:12 +0900

    서울, 수원 지역 해시태그 조회

--- src/main/java/com/promenade/promenadeapp/domain/Road/RoadHashtagRepository.java
+++ src/main/java/com/promenade/promenadeapp/domain/Road/RoadHashtagRepository.java
@@ -14,7 +14,8 @@ public interface RoadHashtagRepository extends JpaRepository<RoadHashtag, Long> 
     @Query(value = "SELECT DISTINCT hashtag FROM road_hashtag WHERE road_id=:roadId", nativeQuery = true)
     List<String> findDistinctHashtagByRoadId(@Param("roadId") Long roadId);
 
-    @Query("select h from RoadHashtag h where h.hashtag=:hashtag and (h.road.startLotAddr like '%수원%' or h.road.startRoadAddr like '%수원%')")
-    List<RoadHashtag> findByHashtag(String hashtag); // 우선 수원 지역 해시태그 조회 기능만 제공
+    @Query("select h from RoadHashtag h where h.hashtag=:hashtag and (h.road.startLotAddr like '%수원%' or h.road.startRoadAddr like '%수원%'" +
+            "or h.road.startLotAddr like '%서울%' or h.road.startRoadAddr like '%서울%')")
+    List<RoadHashtag> findByHashtag(String hashtag); // 우선 서울과 수원 지역 해시태그 조회 기능만 제공
 
 }
