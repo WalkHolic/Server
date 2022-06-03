2512d211d28820b6fbd302e96e956dfe14cb63c2
Sion Noh <shtldhs123@ajou.ac.kr>
2022-06-03 18:24:55 +0900

    리뷰에 네임 추가

--- src/main/java/com/promenade/promenadeapp/dto/Park/ParkReviewResponseDto.java
+++ src/main/java/com/promenade/promenadeapp/dto/Park/ParkReviewResponseDto.java
@@ -16,6 +16,7 @@ public class ParkReviewResponseDto {
     private Long userId;
     private String userName;
     private Long parkId;
+    private String name;
     private double score;
     private String content;
     private String pngPath;
@@ -27,6 +28,7 @@ public class ParkReviewResponseDto {
         this.userId = parkReview.getUser().getId();
         this.userName = parkReview.getUser().getName();
         this.parkId = parkReview.getPark().getId();
+        this.name = parkReview.getPark().getName();
         this.score = parkReview.getScore();
         this.content = parkReview.getContent();
         this.pngPath = parkReview.getPngPath();
